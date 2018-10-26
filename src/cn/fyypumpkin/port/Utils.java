a15c51c86288fe05ad5f0f2201a287cf1afbbb97
fyypumpkin <fyypumpkin@gmail.com>
2018-10-26 18:52:43 +0800

    forward

--- src/cn/fyypumpkin/port/Utils.java
+++ src/cn/fyypumpkin/port/Utils.java
@@ -1,6 +1,6 @@
 package cn.fyypumpkin.port;
 
-import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
 
 /**
  * @author fyypumpkin on 2018/10/26
@@ -8,7 +8,7 @@ import java.nio.channels.SocketChannel;
 public class Utils {
     public static void print(Object left, Object right) {
         String format = "%-" + (20 - left.toString().length()) + "s";
-        System.out.printf(format, left);
-        System.out.println(right);
+        System.out.printf(format, new String(left.toString().getBytes(), Charset.forName("utf-8")));
+        System.out.println(new String(right.toString().getBytes(), Charset.forName("utf-8")));
     }
 }
